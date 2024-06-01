7cb864a05193e55e83282e0c4437643c0457b208
iry18 <155373489+iry18@users.noreply.github.com>
2024-06-01 16:23:45 +0200

    ID- 5 resolved

---
+++ src/main/java/control/pcontrol.java
@@ -0,0 +1,24 @@
+package control;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebFilter("/pcontrol")
+public class pcontrol implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code, if any
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Content-Security-Policy", "directive-value");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code, if any
+    }
+}
